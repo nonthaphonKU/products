26c6e07b3a613d0d1b3cf7cf3caba0463ba34f40
nonthaphonKU <nonthaphon.v@ku.th>
2022-11-15 15:43:14 +0700

    From Class

---
+++ listproducts.py
@@ -0,0 +1,9 @@
+from pymongo import MongoClient
+
+client = MongoClient("theory.cpe.ku.ac.th", username="user07", password="pass07")
+db = client.db07
+products = db.products
+
+for p in products.find():
+    print(p["name"], p["price"])
+
